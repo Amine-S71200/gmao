721686d4d340942eb68bbedf5e9fb8352d2ea542
Amine Hammouda <amine.hammoud51@gmail.com>
2025-08-18 01:00:55 +0200

    Mise à jour hooks.py avec chemins CSS/JS front-end et email

--- gmao/hooks.py
+++ gmao/hooks.py
@@ -1,4 +1,12 @@
-app_name = "gmao"
+﻿app_name = "gmao"
 app_title = "Gmao"
 app_publisher = "Amine"
-app_description = "application de maintenance"
+app_description = "Application de maintenance"
+app_icon = "octicon octicon-tools"
+app_color = "blue"
+app_email = "amine.hammoud51@gmail.com"
+app_license = "MIT"
+
+# 🔹 Assets front-end
+app_include_css = "/assets/gmao/css/gmao.css"
+app_include_js = "/assets/gmao/js/gmao.js"
